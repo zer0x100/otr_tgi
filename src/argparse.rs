56dfb7e1b60c02f04846162e2ace6c18028bdc6c
unknown <vikingzer0x100@gmail.com>
2023-11-22 15:42:39 +0900

    add otr_point argument

--- src/argparse.rs
+++ src/argparse.rs
@@ -11,16 +11,18 @@ pub struct Args {
     /// The number of test samples.
     #[arg(long)]
     pub sample_size: usize,
+    /// Otr point.
+    #[arg(long)]
+    pub otr_point: usize,
     /// Reference signals file(csv)
     ///
     /// 2d csv file required.(i, j) is a value of j-th time in i-th sample.
     #[arg(long)]
     pub references: String,
-    /// Otr point and values file.
+    /// Otr values file.
     ///
-    /// 2d csv file required.
-    /// 0-th row: (0, 0) is the otr point.
-    /// 1-th row: let i>=0, (0, i) is a value of the otr point in the i-th sample.
+    /// 1d csv file required.
+    /// (0, i) is a value of the otr point in the i-th sample.
     #[arg(long)]
     pub otr_point_values: String,
     ///Slow Step Function File
